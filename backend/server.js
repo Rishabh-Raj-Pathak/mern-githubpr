2b7bb09d53bd042e8f8e3735ebaca53392f18991
rishabhrajpathak06@gmail.com <rishabhrajpathak06@gmail.com>
2024-03-16 18:11:47 +0530

    dep

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import passport from "passport";
 import session from "express-session";
+import path from "path"
 import "./passport/github.auth.js"
 
 import userRoutes from "./routes/user.routes.js";
@@ -14,6 +15,9 @@ import connectMongoDB from "./db/connectMongoDB.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT||5000;
+const __dirname = path.resolve();
+
 app.use(session({secret:'keyboard cat',resave:false,saveUninitialized:false}));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -28,7 +32,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
-app.listen(5000, () => {
-  console.log("server started on local host:5000");
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "backend", "dist", "index.html"))
+})
+
+app.listen(PORT, () => {
+  console.log(`server started on local host:${PORT}`);
   connectMongoDB();
 });
